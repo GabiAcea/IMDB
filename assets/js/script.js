f11f30e168ec904d127b265e09810d66e65d0260
GabiAcea <acea.gabriel@gmail.com>
2016-10-18 22:18:11 +0300

    top_bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 console.log('here');
 $.ajax({
-  url: "http://www.omdbapi.com/?t=terminator&y=&plot=short&r=json",
+  url: "http://www.omdbapi.com/?t=unborn&y=&plot=short&r=json",
   cache: false,
   success: function(data){
     console.log(data);
